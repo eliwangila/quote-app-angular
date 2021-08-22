cc3b383151488bf4ece634048065bd21d9652d60
eliwangila <ekirapaeli254@gmail.com>
2021-08-22 08:46:24 +0300

    modified quote-form ts file

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote-form',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteFormComponent implements OnInit {
 
+  newQuote = new Quote(0,"","","",new Date(),0,0);
+  
+  @Output() addQuote = new EventEmitter<Quote>();
+ 
+  submitQuote(f:NgForm){
+    if(f.valid){
+      this.addQuote.emit(this.newQuote);
+      this.newQuote = new Quote(0,"","","",new Date(),0,0);
+    }else{
+      alert("Please Fill All the Fields")
+    }
+
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
