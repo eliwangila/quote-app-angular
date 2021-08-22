5f005ef7ba9c199226019acc99d6c46d03c08c6c
eliwangila <ekirapaeli254@gmail.com>
2021-08-22 08:18:10 +0300

    add properties to class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,11 @@
 export class Quote {
+    public showDescription: boolean;
+    constructor(public id:number,
+      public name:string,
+      public quote:string,
+      public author:string, 
+      public datePosted: Date,
+      public likes:number, 
+      public dislikes:number){
+        this.showDescription=false;}
 }
