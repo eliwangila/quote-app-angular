3cbdd4e63a96cabd3f01fef5fc4800fd3cad2397
eliwangila <ekirapaeli254@gmail.com>
2021-08-23 11:13:29 +0300

    modified dateCount

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,12 +9,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1, 'William', 'There is always some madness in love. But there is also always some reason in madness.', 'Friedrich Nietzsche', new Date(), 0, 0),
-    new Quote(2, 'Mercy', 'The circumstances surrounding your birth is not as important as the opportunity to live life.', 'Lailah Gifty Akita', new Date(), 0, 0),
-    new Quote(3, 'Mark', 'Understanding is the first step to acceptance, and only with acceptance can there be recovery.', 'J.K. Rowling', new Date(), 0, 0),
-    new Quote(4, 'Joseph', 'I don’t agree that when you love, you are blind or fool. You just get wiser and see clearer what is best and of worth.', 'Hark Herald Sarmiento', new Date(), 0, 0),
-    new Quote(5, 'Josephine', 'True beauty is measured by the number of pearls within you, not those around your neck.', 'Suzy Kassem', new Date(), 0, 0),
-    new Quote(6, 'Cherotich', 'Toleration is the greatest gift of the mind; it requires the same effort of the brain that it takes to balance oneself on a bicycle.', 'Helen Keller', new Date(), 0, 0)
+    new Quote(1, 'William', 'There is always some madness in love. But there is also always some reason in madness.', 'Friedrich Nietzsche', new Date(2021,7,17), 0, 0),
+    new Quote(2, 'Mercy', 'The circumstances surrounding your birth is not as important as the opportunity to live life.', 'Lailah Gifty Akita', new Date(2021,7,18), 0, 0),
+    new Quote(3, 'Mark', 'Understanding is the first step to acceptance, and only with acceptance can there be recovery.', 'J.K. Rowling', new Date(2021,7,19), 0, 0),
+    new Quote(4, 'Joseph', 'I don’t agree that when you love, you are blind or fool. You just get wiser and see clearer what is best and of worth.', 'Hark Herald Sarmiento', new Date(2021,7,20), 0, 0),
+    new Quote(5, 'Josephine', 'True beauty is measured by the number of pearls within you, not those around your neck.', 'Suzy Kassem', new Date(2021,7,21), 0, 0),
+    new Quote(6, 'Cherotich', 'Toleration is the greatest gift of the mind; it requires the same effort of the brain that it takes to balance oneself on a bicycle.', 'Helen Keller', new Date(2021,7,22), 0, 0)
 
   ];
   get sortQuotes() {
